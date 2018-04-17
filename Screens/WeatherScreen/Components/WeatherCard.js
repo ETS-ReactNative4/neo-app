50b14ce509875338dea26608b677f7fb39860c37
dani-PYT <dani@pickyourtrail.com>
2018-04-17 20:02:55 +0530

    create weather card

---
+++ Screens/WeatherScreen/Components/WeatherCard.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+} from 'react-native';
+import PropTypes from 'prop-types';
+import constants from "../../../constants/constants";
+
+const WeatherCard = ({location, date, description, temperature, weatherIcon, containerStyle}) => {
+
+  if(!containerStyle) containerStyle = {};
+
+  return (
+    <View style={[styles.weatherCardContainer, containerStyle]}>
+      <View style={styles.textContainer}>
+        <Text style={styles.locationText}>{location}</Text>
+        <Text style={styles.dateText}>{date}</Text>
+        <Text style={styles.descriptionText}>{description}</Text>
+      </View>
+      <View style={styles.imageContainer}>
+        <Text style={styles.temperature}>{temperature}</Text>
+        <Image
+          style={styles.temperatureIcon}
+          source={weatherIcon}
+        />
+      </View>
+    </View>
+  )
+};
+
+WeatherCard.propTypes = {
+  location: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  temperature: PropTypes.string.isRequired,
+  weatherIcon: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.number,
+  ]).isRequired,
+  containerStyle: PropTypes.object,
+};
+
+const styles = StyleSheet.create({
+  weatherCardContainer: {
+    marginVertical: 24,
+    height: 72,
+    flexDirection: 'row',
+  },
+  textContainer: {
+
+  },
+  locationText: {
+    ...constants.font20(constants.primaryLight),
+    color: constants.black1,
+  },
+  dateText: {
+    ...constants.font17(constants.primaryLight),
+    color: 'rgba(121,5,114,1)',
+  },
+  descriptionText: {
+    ...constants.font13(constants.primaryLight),
+    color: constants.black2,
+  },
+  imageContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-end',
+  },
+  temperature: {
+    ...constants.font30(constants.primaryLight),
+    color: constants.black1,
+  },
+  temperatureIcon: {
+    height: 46,
+    width: 46,
+  },
+});
+
+export default WeatherCard;
--- Screens/WeatherScreen/Weather.js
+++ Screens/WeatherScreen/Weather.js
@@ -3,14 +3,46 @@ import React, {
 } from 'react';
 import {
   View,
+  StyleSheet,
+  ScrollView,
 } from 'react-native';
+import WeatherCard from "./Components/WeatherCard";
+import constants from "../../constants/constants";
 
 class Weather extends Component {
+
+  static navigationOptions = {
+    title: 'Weather',
+    tabBarVisible: false,
+  };
+
+  state = {
+    selectedWeatherInfo: {
+      location: 'Ubud, Indonesia',
+      date: 'Today, Dec 9, 12pm',
+      description: 'Feels like 42˚, partly cloudy',
+      temperature: '27˚',
+      weatherIcon: constants.notificationIcon,
+    },
+  };
+
   render() {
     return (
-      <View/>
-    )
+      <ScrollView style={styles.weatherContainer}>
+        <WeatherCard
+          {...this.state.selectedWeatherInfo}
+        />
+      </ScrollView>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  weatherContainer: {
+    flex: 1,
+    paddingHorizontal: 24,
+    backgroundColor: 'white',
+  },
+});
 
 export default Weather;
