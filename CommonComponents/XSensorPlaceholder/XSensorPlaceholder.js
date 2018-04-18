c35df2dcacaadeb26f1833f440b63a47a9963c61
dani-PYT <dani@pickyourtrail.com>
2018-04-18 13:21:22 +0530

    testing prettier

--- CommonComponents/XSensorPlaceholder/XSensorPlaceholder.js
+++ CommonComponents/XSensorPlaceholder/XSensorPlaceholder.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import {View,} from 'react-native';
-import PropTypes from 'prop-types';
+import React from "react";
+import { View } from "react-native";
+import PropTypes from "prop-types";
 
-const XSensorPlaceholder = ({containerStyle}) => {if(!containerStyle) containerStyle = {};return (<View style={[{height: 30}, containerStyle]}/>);};
+const XSensorPlaceholder = ({ containerStyle }) => {
+  if (!containerStyle) containerStyle = {};
+  return <View style={[{ height: 30 }, containerStyle]} />;
+};
 
-XSensorPlaceholder.propTypes = {containerStyle: PropTypes.object,};
+XSensorPlaceholder.propTypes = { containerStyle: PropTypes.object };
 
 export default XSensorPlaceholder;
