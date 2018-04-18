b3573266c2dd9f5d92bf140d7980a5d0c040fe3a
dani-PYT <dani@pickyourtrail.com>
2018-04-18 13:22:13 +0530

    testing prettier with weathercard

--- Screens/WeatherScreen/Components/WeatherCard.js
+++ Screens/WeatherScreen/Components/WeatherCard.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-} from 'react-native';
-import PropTypes from 'prop-types';
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import PropTypes from "prop-types";
 import constants from "../../../constants/constants";
 
-const WeatherCard = ({location, date, description, temperature, weatherIcon, containerStyle}) => {
-
-  if(!containerStyle) containerStyle = {};
+const WeatherCard = ({
+  location,
+  date,
+  description,
+  temperature,
+  weatherIcon,
+  containerStyle
+}) => {
+  if (!containerStyle) containerStyle = {};
 
   return (
     <View style={[styles.weatherCardContainer, containerStyle]}>
@@ -21,13 +22,10 @@ const WeatherCard = ({location, date, description, temperature, weatherIcon, con
       </View>
       <View style={styles.imageContainer}>
         <Text style={styles.temperature}>{temperature}</Text>
-        <Image
-          style={styles.temperatureIcon}
-          source={weatherIcon}
-        />
+        <Image style={styles.temperatureIcon} source={weatherIcon} />
       </View>
     </View>
-  )
+  );
 };
 
 WeatherCard.propTypes = {
@@ -35,48 +33,44 @@ WeatherCard.propTypes = {
   date: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   temperature: PropTypes.string.isRequired,
-  weatherIcon: PropTypes.oneOfType([
-    PropTypes.object,
-    PropTypes.number,
-  ]).isRequired,
-  containerStyle: PropTypes.object,
+  weatherIcon: PropTypes.oneOfType([PropTypes.object, PropTypes.number])
+    .isRequired,
+  containerStyle: PropTypes.object
 };
 
 const styles = StyleSheet.create({
   weatherCardContainer: {
     marginVertical: 24,
     height: 72,
-    flexDirection: 'row',
+    flexDirection: "row"
   },
-  textContainer: {
-
-  },
+  textContainer: {},
   locationText: {
     ...constants.font20(constants.primaryLight),
-    color: constants.black1,
+    color: constants.black1
   },
   dateText: {
     ...constants.font17(constants.primaryLight),
-    color: 'rgba(121,5,114,1)',
+    color: "rgba(121,5,114,1)"
   },
   descriptionText: {
     ...constants.font13(constants.primaryLight),
-    color: constants.black2,
+    color: constants.black2
   },
   imageContainer: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-end',
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "flex-end"
   },
   temperature: {
     ...constants.font30(constants.primaryLight),
-    color: constants.black1,
+    color: constants.black1
   },
   temperatureIcon: {
     height: 46,
-    width: 46,
-  },
+    width: 46
+  }
 });
 
 export default WeatherCard;
