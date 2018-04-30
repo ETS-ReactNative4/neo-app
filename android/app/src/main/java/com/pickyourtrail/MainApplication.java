7287f71fc7784f239158e0dcd3498cc911828895
dani-PYT <dani@pickyourtrail.com>
2018-04-30 14:51:05 +0530

    install sms listener

--- android/app/src/main/java/com/pickyourtrail/MainApplication.java
+++ android/app/src/main/java/com/pickyourtrail/MainApplication.java
@@ -3,6 +3,7 @@ package com.pickyourtrail;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.centaurwarchief.smslistener.SmsListenerPackage;
 import com.oblador.keychain.KeychainPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SmsListenerPackage(),
             new KeychainPackage(),
             new SvgPackage()
       );
