cc7729f998f26681f701c2cb7ff808c3cd875a5e
dani-PYT <dani@pickyourtrail.com>
2018-05-11 17:07:07 +0530

    change starter background that caused lagging

--- constants/imageAssets.js
+++ constants/imageAssets.js
@@ -1,6 +1,6 @@
 const imageAssets = {
   splashBackground: require("../assets/images/backgrounds/viceroy-bali.jpg.jpg"),
-  starterBackground: require("../assets/images/backgrounds/awesome-natural-photo-for-desktop-1229x768.jpg"),
+  starterBackground: require("../assets/images/backgrounds/viceroy-bali.jpg.jpg"),
   notificationIcon: require("../assets/images/icons/notification.png"),
   backArrow: require("../assets/images/icons/left-arrow.png"),
   dropDownArrow: require("../assets/images/icons/drop-down-arrow.png"),
