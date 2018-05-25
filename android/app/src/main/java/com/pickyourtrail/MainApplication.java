6683fac0477b079c1d1084b53ab622bfcc2d7adf
dani-PYT <dani@pickyourtrail.com>
2018-05-25 12:18:50 +0530

    install react native vector icons

--- android/app/src/main/java/com/pickyourtrail/MainApplication.java
+++ android/app/src/main/java/com/pickyourtrail/MainApplication.java
@@ -3,6 +3,7 @@ package com.pickyourtrail;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.centaurwarchief.smslistener.SmsListenerPackage;
 import com.oblador.keychain.KeychainPackage;
 import com.horcrux.svg.SvgPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new SmsListenerPackage(),
             new KeychainPackage(),
             new SvgPackage()
