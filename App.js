4451a2ace12475a93a81e2e06c7fb99ea6e8ff0f
dani-PYT <dani@pickyourtrail.com>
2018-05-31 19:23:34 +0530

    create error logger

--- App.js
+++ App.js
@@ -30,15 +30,6 @@ import HotelVoucher from "./Screens/VoucherScreens/HotelVoucherScreen/HotelVouch
 import HomeTabBar from "./CommonComponents/HomeTabBar/HomeTabBar";
 import ActivityVoucher from "./Screens/VoucherScreens/ActivityVoucherScreen/ActivityVoucher";
 import FlightVoucher from "./Screens/VoucherScreens/FlightVoucherScreen/FlightVoucher";
-import { Sentry } from "react-native-sentry";
-
-Sentry.config(
-  "https://af21f52962874bf49a888d1826e5a827:dbbf31a3751e420d8d579f33139558fc@sentry.io/1216774"
-).install();
-
-Sentry.captureException(new Error(`Test Error! - ${Platform.OS}`), {
-  logger: "my.module"
-});
 
 UIManager.setLayoutAnimationEnabledExperimental &&
   UIManager.setLayoutAnimationEnabledExperimental(true);
