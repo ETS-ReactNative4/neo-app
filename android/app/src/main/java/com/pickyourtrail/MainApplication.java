f489b1ba6b76018d69fb47306ec07c11ec246361
dani-PYT <dani@pickyourtrail.com>
2018-08-16 16:20:33 +0530

    install upgraded sms listener and add sms permission service

--- android/app/src/main/java/com/pickyourtrail/MainApplication.java
+++ android/app/src/main/java/com/pickyourtrail/MainApplication.java
@@ -3,6 +3,7 @@ package com.pickyourtrail;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.centaurwarchief.smslistener.SmsListenerPackage;
 import com.krazylabs.OpenAppSettingsPackage;
 import net.no_mad.tts.TextToSpeechPackage;
 import io.sentry.RNSentryPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SmsListenerPackage(),
             new OpenAppSettingsPackage(),
             new TextToSpeechPackage(),
             new RNSentryPackage(),
