ae582220cd1629acaa8435c0086eb013956f08dd
dani-PYT <dani@pickyourtrail.com>
2018-08-16 16:18:42 +0530

    remove ignore function for yellow box warning

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 import { AppRegistry } from "react-native";
 import App from "./App";
 
-/**
- * TODO: Find a way to overcome this warning
- */
-import { YellowBox } from "react-native";
-YellowBox.ignoreWarnings([
-  "Warning: isMounted(...) is deprecated",
-  "Module RCTImageLoader"
-]);
-
 AppRegistry.registerComponent("Pickyourtrail", () => App);
