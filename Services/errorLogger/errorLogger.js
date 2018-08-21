d8c0ce7a5533fc6ae0a8b4248c186d7f16b5bb84
dani-pyt <dani@pickyourtrail.com>
2018-08-21 15:33:23 +0530

    sentry include environment info in logs

--- Services/errorLogger/errorLogger.js
+++ Services/errorLogger/errorLogger.js
@@ -1,10 +1,13 @@
 import { Sentry as sentry } from "react-native-sentry";
+import PackageInfo from "../../package.json";
 
 sentry
   .config(
     "https://af21f52962874bf49a888d1826e5a827:dbbf31a3751e420d8d579f33139558fc@sentry.io/1216774",
     {
-      captureUnhandledRejections: true
+      captureUnhandledRejections: true,
+      release: `${PackageInfo.version} build ${PackageInfo.build}`,
+      environment: PackageInfo.environment
     }
   )
   .install();
