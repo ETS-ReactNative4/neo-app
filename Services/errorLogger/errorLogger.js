3ce374a16fe4e17f9230fc1f8493b68a3723c812
dani-pyt <dani@pickyourtrail.com>
2018-09-06 16:12:03 +0530

    disable sentry in dev environments

--- Services/errorLogger/errorLogger.js
+++ Services/errorLogger/errorLogger.js
@@ -1,11 +1,13 @@
 import { Sentry as sentry } from "react-native-sentry";
 import PackageInfo from "../../package.json";
 
-sentry
-  .config(
-    "https://af21f52962874bf49a888d1826e5a827:dbbf31a3751e420d8d579f33139558fc@sentry.io/1216774"
-  )
-  .install();
+if (!__DEV__) {
+  sentry
+    .config(
+      "https://af21f52962874bf49a888d1826e5a827:dbbf31a3751e420d8d579f33139558fc@sentry.io/1216774"
+    )
+    .install();
+}
 
 sentry.setTagsContext({
   environment: PackageInfo.environment
