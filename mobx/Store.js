ee293b36fd8513c1e1fb751606a050f63d470545
dani-pyt <dani@pickyourtrail.com>
2018-09-07 13:39:46 +0530

    convert store into a function

--- mobx/Store.js
+++ mobx/Store.js
@@ -10,106 +10,112 @@ import Voucher from "./Voucher";
 import { logError } from "../Services/errorLogger/errorLogger";
 import Phrases from "./Phrases";
 
-const store = {
-  userStore: new User(),
-  yourBookingsStore: new YourBookings(),
-  appState: new AppState(),
-  itineraries: new Itineraries(),
-  weatherStore: new Weather(),
-  packingChecklistStore: new PackingChecklist(),
-  voucherStore: new Voucher(),
-  phrasesStore: new Phrases()
+const createStore = () => {
+  const appStore = {
+    userStore: new User(),
+    yourBookingsStore: new YourBookings(),
+    appState: new AppState(),
+    itineraries: new Itineraries(),
+    weatherStore: new Weather(),
+    packingChecklistStore: new PackingChecklist(),
+    voucherStore: new Voucher(),
+    phrasesStore: new Phrases()
+  };
+
+  const hydrate = create({
+    storage: AsyncStorage,
+    jsonify: true
+  });
+
+  hydrate("_upcomingItineraries", appStore.yourBookingsStore)
+    .then(() => {})
+    .catch(err => {
+      logError(err);
+    });
+  hydrate("_completedItineraries", appStore.yourBookingsStore)
+    .then(() => {})
+    .catch(err => {
+      logError(err);
+    });
+  hydrate("_itineraries", appStore.itineraries)
+    .then(() => {})
+    .catch(err => {
+      logError(err);
+    });
+  hydrate("_selectedItinerary", appStore.itineraries)
+    .then(() => {})
+    .catch(err => {
+      logError(err);
+    });
+  hydrate("_vouchers", appStore.voucherStore)
+    .then(() => {})
+    .catch(err => {
+      logError(err);
+    });
+  hydrate("_selectedVoucher", appStore.voucherStore)
+    .then(() => {})
+    .catch(err => {
+      logError(err);
+    });
+  hydrate("_tripMode", appStore.appState)
+    .then(() => {})
+    .catch(err => {
+      logError(err);
+    });
+  hydrate("_weather", appStore.weatherStore)
+    .then(() => {})
+    .catch(err => {
+      logError(err);
+    });
+  hydrate("_conversionRates", appStore.appState)
+    .then(() => {})
+    .catch(err => {
+      logError(err);
+    });
+  hydrate("_allPackingChecklists", appStore.packingChecklistStore)
+    .then(() => {})
+    .catch(err => {
+      logError(err);
+    });
+  hydrate("_user", appStore.userStore)
+    .then(() => {})
+    .catch(err => {
+      logError(err);
+    });
+  hydrate("_phrases", appStore.phrasesStore)
+    .then(() => {})
+    .catch(err => {
+      logError(err);
+    });
+  hydrate("_translatedPhrases", appStore.phrasesStore)
+    .then(() => {})
+    .catch(err => {
+      logError(err);
+    });
+  hydrate("_translatedPhrase", appStore.phrasesStore)
+    .then(() => {})
+    .catch(err => {
+      logError(err);
+    });
+  hydrate("_languages", appStore.phrasesStore)
+    .then(() => {})
+    .catch(err => {
+      logError(err);
+    });
+  hydrate("_selectedPhrase", appStore.phrasesStore)
+    .then(() => {})
+    .catch(err => {
+      logError(err);
+    });
+  hydrate("_pinnedPhrases", appStore.phrasesStore)
+    .then(() => {})
+    .catch(err => {
+      logError(err);
+    });
+
+  return appStore;
 };
 
-const hydrate = create({
-  storage: AsyncStorage,
-  jsonify: true
-});
-
-hydrate("_upcomingItineraries", store.yourBookingsStore)
-  .then(() => {})
-  .catch(err => {
-    logError(err);
-  });
-hydrate("_completedItineraries", store.yourBookingsStore)
-  .then(() => {})
-  .catch(err => {
-    logError(err);
-  });
-hydrate("_itineraries", store.itineraries)
-  .then(() => {})
-  .catch(err => {
-    logError(err);
-  });
-hydrate("_selectedItinerary", store.itineraries)
-  .then(() => {})
-  .catch(err => {
-    logError(err);
-  });
-hydrate("_vouchers", store.voucherStore)
-  .then(() => {})
-  .catch(err => {
-    logError(err);
-  });
-hydrate("_selectedVoucher", store.voucherStore)
-  .then(() => {})
-  .catch(err => {
-    logError(err);
-  });
-hydrate("_tripMode", store.appState)
-  .then(() => {})
-  .catch(err => {
-    logError(err);
-  });
-hydrate("_weather", store.weatherStore)
-  .then(() => {})
-  .catch(err => {
-    logError(err);
-  });
-hydrate("_conversionRates", store.appState)
-  .then(() => {})
-  .catch(err => {
-    logError(err);
-  });
-hydrate("_allPackingChecklists", store.packingChecklistStore)
-  .then(() => {})
-  .catch(err => {
-    logError(err);
-  });
-hydrate("_user", store.userStore)
-  .then(() => {})
-  .catch(err => {
-    logError(err);
-  });
-hydrate("_phrases", store.phrasesStore)
-  .then(() => {})
-  .catch(err => {
-    logError(err);
-  });
-hydrate("_translatedPhrases", store.phrasesStore)
-  .then(() => {})
-  .catch(err => {
-    logError(err);
-  });
-hydrate("_translatedPhrase", store.phrasesStore)
-  .then(() => {})
-  .catch(err => {
-    logError(err);
-  });
-hydrate("_languages", store.phrasesStore)
-  .then(() => {})
-  .catch(err => {
-    logError(err);
-  });
-hydrate("_selectedPhrase", store.phrasesStore)
-  .then(() => {})
-  .catch(err => {
-    logError(err);
-  });
-hydrate("_pinnedPhrases", store.phrasesStore)
-  .then(() => {})
-  .catch(err => {
-    logError(err);
-  });
+const store = createStore();
 
 export default store;
