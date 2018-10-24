521683dc1d7c36c83a220f2ae7eba9371f16ce86
dani-pyt <dani@pickyourtrail.com>
2018-10-24 18:26:32 +0530

    disable fcm temporarily

--- Services/fcmService/fcm.js
+++ Services/fcmService/fcm.js
@@ -1,19 +1,19 @@
-import FCM, { FCMEvent } from "react-native-fcm";
+// import FCM, { FCMEvent } from "react-native-fcm";
 import { logError } from "../errorLogger/errorLogger";
 import storeService from "../storeService/storeService";
 
 export const getDeviceToken = async () => {
   try {
-    await FCM.requestPermissions({ badge: true, sound: true, alert: true });
-    const token = await FCM.getFCMToken();
-    storeService.appState.setPushTokens(token);
+    // await FCM.requestPermissions({ badge: true, sound: true, alert: true });
+    // const token = await FCM.getFCMToken();
+    storeService.appState.setPushTokens("token");
   } catch (err) {
     logError(err);
   }
 };
 
 export const registerFcmRefreshListener = () => {
-  FCM.on(FCMEvent.RefreshToken, token => {
-    storeService.appState.setPushTokens(token);
-  });
+  // FCM.on(FCMEvent.RefreshToken, token => {
+  //   storeService.appState.setPushTokens(token);
+  // });
 };
