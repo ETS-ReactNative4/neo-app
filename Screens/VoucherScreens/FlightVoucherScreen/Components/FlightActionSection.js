5a4f868bcc1c7886a4c8af6e45964c3f84b6e018
dani-pyt <dani@pickyourtrail.com>
2018-11-15 19:08:30 +0530

    remove flight status button

--- Screens/VoucherScreens/FlightVoucherScreen/Components/FlightActionSection.js
+++ Screens/VoucherScreens/FlightVoucherScreen/Components/FlightActionSection.js
@@ -4,26 +4,27 @@ import { NavigationActions } from "react-navigation";
 import SimpleButton from "../../../../CommonComponents/SimpleButton/SimpleButton";
 import navigationService from "../../../../Services/navigationService/navigationService";
 import constants from "../../../../constants/constants";
+import { responsiveWidth } from "react-native-responsive-dimensions";
 
 const openStatus = NavigationActions.navigate({ routeName: "FlightStatus" });
 const FlightActionSection = () => {
   return (
     <View style={styles.flightActionSection}>
-      <SimpleButton
-        text={"Status"}
-        action={() => navigationService.navigation.dispatch(openStatus)}
-        textColor={constants.firstColor}
-        color={"white"}
-        hasBorder={true}
-        containerStyle={{ height: 40, width: 158 }}
-      />
+      {/*<SimpleButton*/}
+      {/*text={"Status"}*/}
+      {/*action={() => navigationService.navigation.dispatch(openStatus)}*/}
+      {/*textColor={constants.firstColor}*/}
+      {/*color={"white"}*/}
+      {/*hasBorder={true}*/}
+      {/*containerStyle={{ height: 40, width: 158 }}*/}
+      {/*/>*/}
       <SimpleButton
         text={"Web Checkin"}
         action={() => null}
         textColor={constants.firstColor}
         color={"white"}
         hasBorder={true}
-        containerStyle={{ height: 40, width: 158 }}
+        containerStyle={{ height: 40, width: responsiveWidth(100) - 48 }}
       />
     </View>
   );
@@ -32,7 +33,7 @@ const FlightActionSection = () => {
 const styles = StyleSheet.create({
   flightActionSection: {
     flexDirection: "row",
-    justifyContent: "space-between"
+    justifyContent: "center"
   }
 });
 
