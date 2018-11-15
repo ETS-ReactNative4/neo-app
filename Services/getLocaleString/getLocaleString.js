f4c496cf08a12c9faf4403f4799006486de77219
dani-pyt <dani@pickyourtrail.com>
2018-11-15 16:44:44 +0530

    handle strings in getLocaleString service

--- Services/getLocaleString/getLocaleString.js
+++ Services/getLocaleString/getLocaleString.js
@@ -1,6 +1,12 @@
-const getLocaleString = amount =>
-  new Intl.NumberFormat("en-IN", { style: "currency", currency: "INR" }).format(
-    amount
-  );
+const getLocaleString = amount => {
+  if (typeof amount === "number") {
+    return new Intl.NumberFormat("en-IN", {
+      style: "currency",
+      currency: "INR"
+    }).format(amount);
+  } else {
+    return `₹ ${amount}`;
+  }
+};
 
 export default getLocaleString;
