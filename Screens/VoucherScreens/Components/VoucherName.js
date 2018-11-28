e19c6062a9ca318de0405d02d8c34a10eccb96fb
dani-pyt <dani@pickyourtrail.com>
2018-11-28 21:56:29 +0530

    set voucher name to title case

--- Screens/VoucherScreens/Components/VoucherName.js
+++ Screens/VoucherScreens/Components/VoucherName.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 import constants from "../../../constants/constants";
 import PropTypes from "prop-types";
+import getTitleCase from "../../../Services/getTitleCase/getTitleCase";
 
 const VoucherName = ({ name, textStyle }) => {
   if (!textStyle) textStyle = {};
@@ -11,7 +12,7 @@ const VoucherName = ({ name, textStyle }) => {
       numberOfLines={4}
       ellipsizeMode={"tail"}
     >
-      {name}
+      {getTitleCase(name)}
     </Text>
   );
 };
