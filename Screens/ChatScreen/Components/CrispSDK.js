7d530e7387671d08950c22ec4b436d10bfe77236
dani-pyt <dani@pickyourtrail.com>
2018-12-03 12:56:51 +0530

    comment out crisp sdk for fallback

--- Screens/ChatScreen/Components/CrispSDK.js
+++ Screens/ChatScreen/Components/CrispSDK.js
@@ -1,14 +1,15 @@
-const CrispSDK = `
-  setTimeout(() => {
-    var sessionId = $crisp.get("session:identifier");
-    var email = $crisp.get("user:email");
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "https://platodev.pickyourtrail.com/Postapi/CrispSetEmailFallBack?session_id="+sessionId+"&email="+email);
-    xmlhttp.send();
-  }, 10000);
-`;
+import constants from "../../../constants/constants";
+
+const CrispSDK = ``;
 
 // xmlhttp.setRequestHeader( 'Access-Control-Allow-Origin', '*');
+// setTimeout(() => {
+//   var sessionId = $crisp.get("session:identifier");
+//   var email = $crisp.get("user:email");
+//   var xmlhttp = new XMLHttpRequest();
+//   xmlhttp.open("GET", "${constants.platoServerUrl}Postapi/CrispSetEmailFallBack?session_id="+sessionId+"&email="+email);
+//   xmlhttp.send();
+// }, 10000);
 // xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 // xmlhttp.send(JSON.stringify({ "session_id": sessionId, email: email}));
 
