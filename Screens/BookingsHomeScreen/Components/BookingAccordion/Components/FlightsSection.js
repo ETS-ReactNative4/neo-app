a83092a2515ee1761e776392a5d97fba6011aaf8
dani-pyt <dani@pickyourtrail.com>
2018-12-08 13:53:44 +0530

    add 2nd flight timings in fligh section of accordion

--- Screens/BookingsHomeScreen/Components/BookingAccordion/Components/FlightsSection.js
+++ Screens/BookingsHomeScreen/Components/BookingAccordion/Components/FlightsSection.js
@@ -91,8 +91,16 @@ const Flight = ({ flight, isLast, navigation }) => {
       </View>
       <View style={styles.contentTextContainer}>
         <View style={styles.contentHeaderWrapper}>
-          <Text style={styles.contentHeader}>{`${timings[0].departure[0]} - ${
+          <Text
+            style={styles.contentHeader}
+            numberOfLines={2}
+            ellipsizeMode={"tail"}
+          >{`${timings[0].departure[0]} - ${
             timings[0].arrival[timings[0].arrival.length - 1]
+          },${"\n"}${timings[timings.length - 1].departure[0]} - ${
+            timings[timings.length - 1].arrival[
+              timings[timings.length - 1].arrival.length - 1
+            ]
           }`}</Text>
         </View>
         <View style={styles.contentTextWrapper}>
@@ -134,7 +142,7 @@ const styles = StyleSheet.create({
     marginLeft: 16
   },
   contentHeaderWrapper: {
-    height: 16,
+    minHeight: 16,
     justifyContent: "center"
   },
   contentHeader: {
