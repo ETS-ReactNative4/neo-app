ccfd9a70dd4adaf7bc360961a98e4bc066ff0b39
dani-pyt <dani@pickyourtrail.com>
2018-12-10 16:28:39 +0530

    create custom scrollview component

--- CommonComponents/CustomScrollView/CustomScrollView.js
+++ CommonComponents/CustomScrollView/CustomScrollView.js
@@ -4,15 +4,20 @@ import {
   View,
   Text,
   ScrollView,
-  RefreshControl
+  RefreshControl,
+  FlatList
 } from "react-native";
 import PropTypes from "prop-types";
-import { responsiveHeight } from "react-native-responsive-dimensions";
+import constants from "../../constants/constants";
+import LineProgressBar from "../LineProgressBar/LineProgressBar";
 
 class CustomScrollView extends Component {
   static propTypes = {
     children: PropTypes.element.isRequired,
-    onRefresh: PropTypes.func.isRequired
+    onRefresh: PropTypes.func.isRequired,
+    refreshing: PropTypes.bool.isRequired,
+    horizontalPadding: PropTypes.number,
+    scrollComponent: PropTypes.string
   };
   _refreshControlRef = React.createRef();
 
@@ -27,28 +32,47 @@ class CustomScrollView extends Component {
   }
 
   render() {
-    const { children, onRefresh, ...otherProps } = this.props;
+    const {
+      onRefresh,
+      refreshing,
+      horizontalPadding,
+      scrollComponent,
+      ...otherProps
+    } = this.props;
+    otherProps.refreshControl = (
+      <RefreshControl
+        ref={e => (this._refreshControlRef = e)}
+        refreshing={false}
+        tintColor={constants.firstColor}
+        progressBackgroundColor={"white"}
+        colors={[constants.firstColor]}
+        onRefresh={onRefresh}
+      />
+    );
+    let ScrollComponent = null;
+    switch (scrollComponent) {
+      case "ScrollView":
+        ScrollComponent = <ScrollView {...otherProps} />;
+        break;
+      case "FlatList":
+        ScrollComponent = <FlatList {...otherProps} />;
+        break;
+      default:
+        ScrollComponent = <ScrollView {...otherProps} />;
+        break;
+    }
     return (
-      <View style={styles.customScrollContainer}>
-        <ScrollView
-          {...otherProps}
-          refreshControl={
-            <RefreshControl
-              ref={e => (this._refreshControlRef = e)}
-              style={{ backgroundColor: "red", height: 0, width: 0 }}
-              refreshing={false}
-              tintColor="transparent"
-              titleColor="transparent"
-              progressBackgroundColor={"transparent"}
-              colors={["transparent"]}
-              title={"release to refresh!"}
-              progressViewOffset={-responsiveHeight(100)}
-              onRefresh={onRefresh}
-            />
-          }
-        >
-          {children}
-        </ScrollView>
+      <View
+        style={[
+          styles.customScrollContainer,
+          horizontalPadding ? { paddingHorizontal: horizontalPadding } : null
+        ]}
+      >
+        <LineProgressBar
+          isVisible={refreshing}
+          containerStyle={{ marginLeft: -horizontalPadding }}
+        />
+        {ScrollComponent}
       </View>
     );
   }
