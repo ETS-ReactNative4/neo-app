2dcfacd7dcaa90ce1bac834c5defa17238e70708
dani-pyt <dani@pickyourtrail.com>
2019-01-14 17:32:34 +0530

    add core-js polyfill and add proper comments for all polyfills in the app

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+/**
+ * Core-JS polyfill for android
+ * addresses https://github.com/facebook/immutable-js/issues/1305 that only happens in dev mode of Android
+ * added resolutions in package.json based on https://github.com/zloirock/core-js/issues/368#issuecomment-376586585
+ */
+if (Platform.OS === "android" && __DEV__) {
+  require("core-js");
+}
+
+/**
+ * For MobX Support
+ * fix based on https://github.com/facebook/react-native/issues/20150#issue-340235017
+ * For permanent fix need RN 0.57.4 upgrade https://github.com/facebook/react-native/issues/20150#issuecomment-436320490
+ */
 import applyDecoratedDescriptor from "@babel/runtime/helpers/esm/applyDecoratedDescriptor";
 import initializerDefineProperty from "@babel/runtime/helpers/esm/initializerDefineProperty";
 import { Platform } from "react-native";
@@ -9,6 +23,7 @@ Object.assign(babelHelpers, {
 
 /**
  * Intl Polyfill for Android
+ * fix based on https://github.com/facebook/react-native/issues/19410#issuecomment-434232762
  */
 if (Platform.OS === "android") {
   require("intl");
