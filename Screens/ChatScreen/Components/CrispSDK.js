6b77f1addc410930b50cd006b22077ede125c0dc
dani-pyt <dani@pickyourtrail.com>
2019-01-28 19:40:05 +0530

    add threshold to crispfallback recursive call

--- Screens/ChatScreen/Components/CrispSDK.js
+++ Screens/ChatScreen/Components/CrispSDK.js
@@ -1,23 +1,35 @@
 import constants from "../../../constants/constants";
 
+/**
+ * TODO: Find a way to log if threshold limit is reached and fallback failed.
+ */
 const CrispSDK = `
-var callFallback = function () {
+var threshold = 5;
+var callFallback = function() {
   setTimeout(function() {
     var sessionId = $crisp.get("session:identifier");
     var email = $crisp.get("user:email");
     var request = new XMLHttpRequest();
     var url = "${constants.platoServerUrl}Postapi/CrispSetEmailFallBack";
-    if(email) {
+    if (email) {
       if (request.withCredentials !== undefined) {
         request.open("POST", url, true);
         request.setRequestHeader("Access-Control-Allow-Origin", "*");
-        request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        request.send(JSON.stringify({ "session_id": sessionId, email: email}));
+        request.setRequestHeader(
+          "Content-Type",
+          "application/json;charset=UTF-8"
+        );
+        request.send(JSON.stringify({ session_id: sessionId, email: email }));
       } else {
-        request.open("GET", url + "?session_id="+sessionId+"&email="+email, true);
+        request.open(
+          "GET",
+          url + "?session_id=" + sessionId + "&email=" + email,
+          true
+        );
         request.send();
       }
-    } else {
+    } else if (threshold > 0) {
+      threshold--;
       callFallback();
     }
   }, 10000);
