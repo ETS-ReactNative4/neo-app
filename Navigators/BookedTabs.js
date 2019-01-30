3321f1005715f18304558cfa49478e9cc3629b6d
dani-pyt <dani@pickyourtrail.com>
2019-01-30 19:25:52 +0530

    booked tabs set active tab color to theme green

--- Navigators/BookedTabs.js
+++ Navigators/BookedTabs.js
@@ -49,7 +49,7 @@ const BookedTabs = createBottomTabNavigator(
         const { routeName } = navigation.state;
         let icon;
 
-        const color = focused ? constants.black1 : constants.shade1dot5;
+        const color = focused ? constants.firstColor : constants.shade1dot5;
 
         switch (routeName) {
           case "TripFeed":
@@ -78,7 +78,7 @@ const BookedTabs = createBottomTabNavigator(
               icon: focused
                 ? constants.supportSelectedIcon
                 : constants.supportIconLight,
-              color: focused ? constants.firstColor : color
+              color
             };
             break;
 
