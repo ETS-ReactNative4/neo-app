5677a3e523983d03c5f6ca1f9df10c60861c803b
dani-pyt <dani@pickyourtrail.com>
2019-02-14 14:38:11 +0530

    prevent itinerary view from crashing * hides the slot that causes error * Error boundary will capture errorinfo to sentry too

---
+++ Screens/BookedItineraryScreen/Components/SlotActivity/SlotRowWrapper.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { logError } from "../../../../Services/errorLogger/errorLogger";
+import forbidExtraProps from "../../../../Services/PropTypeValidation/forbidExtraProps";
+import PropTypes from "prop-types";
+
+/**
+ * Will catch any errors that prevent a slot from rendering
+ * and hides the slot completely.
+ */
+class SlotRowWrapper extends Component {
+  static propTypes = forbidExtraProps({
+    children: PropTypes.oneOfType([
+      PropTypes.element,
+      PropTypes.arrayOf(PropTypes.element)
+    ]).isRequired,
+    data: PropTypes.object.isRequired
+  });
+
+  state = {
+    hasError: false
+  };
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      hasError: true
+    });
+    logError(error, {
+      type: "Failed to render itinerary view slot row",
+      errorInfo,
+      slotData: this.props.data
+    });
+  }
+
+  render() {
+    if (this.state.hasError) return null;
+    return this.props.children;
+  }
+}
+
+export default SlotRowWrapper;
