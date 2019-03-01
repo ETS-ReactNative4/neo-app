cedaf32259672226dbaf719717e69d6108342c77
dani-pyt <dani@pickyourtrail.com>
2019-03-01 10:14:15 +0530

    remove session merge and load crisp only when email and token are present

--- constants/serverUrls.js
+++ constants/serverUrls.js
@@ -62,11 +62,11 @@ const serverUrls = {
     `https://translate.google.com.vn/translate_tts?ie=UTF-8&q=${phrase}&tl=${language}&client=tw-ob`,
   airlineCdn: "https://d3lf10b5gahyby.cloudfront.net/airline_logos/",
   crispServerUrl: (email, token) =>
-    encodeURI(
-      `https://go.crisp.chat/chat/embed/?website_id=a04827ba-5aa9-4540-866f-e1850a0476eb&user_email=${email}${
-        token ? `&token_id=${token}&session_merge=true` : ""
-      }`
-    ),
+    email && token
+      ? encodeURI(
+          `https://go.crisp.chat/chat/embed/?website_id=a04827ba-5aa9-4540-866f-e1850a0476eb&user_email=${email}&token_id=${token}`
+        )
+      : "",
   offlineContact: "+91 8939891682",
 
   /**
