62fda15fba720bf5a0c1503823b13f25cb914fa6
dani-pyt <dani@pickyourtrail.com>
2019-03-06 14:13:23 +0530

    added comments to isUserLoggedInCallback function

--- Services/isUserLoggedInCallback/isUserLoggedInCallback.js
+++ Services/isUserLoggedInCallback/isUserLoggedInCallback.js
@@ -1,6 +1,10 @@
 import * as Keychain from "react-native-keychain";
 import { logError } from "../errorLogger/errorLogger";
 
+/**
+ * Checks if the user is logged in or not and
+ * fires callbacks to execute the corresponding function
+ */
 const isUserLoggedInCallback = async (
   callbackSuccess = () => null,
   callbackFailure = () => null,
