5725380ec1bfd9ed791f755cb5a84b7ba7935e93
dani-pyt <dani@pickyourtrail.com>
2019-03-17 16:47:31 +0530

    added comments for the validation functions

--- Services/validateMobileNumber/validateMobileNumber.js
+++ Services/validateMobileNumber/validateMobileNumber.js
@@ -1,6 +1,9 @@
 import { isValidNumber } from "libphonenumber-js";
 import { parsePhoneNumberFromString } from "libphonenumber-js/mobile";
 
+/**
+ * Checks if the mobile can exist in the given country code
+ */
 const validateMobileNumber = mobileNumber => {
   return (
     isValidNumber(mobileNumber) &&
@@ -8,6 +11,9 @@ const validateMobileNumber = mobileNumber => {
   );
 };
 
+/**
+ * Only checks the number of valid digits in the mobile number
+ */
 export const validateLoginMobileNumber = mobileNumber =>
   isValidNumber(mobileNumber);
 
