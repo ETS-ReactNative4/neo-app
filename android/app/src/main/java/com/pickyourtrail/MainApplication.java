0b61fe32807dc115d7246d76e7199c36b53a9cb1
dani-PYT <dani@pickyourtrail.com>
2019-06-18 20:24:18 +0530

    enable multidexing in android

--- android/app/src/main/java/com/pickyourtrail/MainApplication.java
+++ android/app/src/main/java/com/pickyourtrail/MainApplication.java
@@ -1,6 +1,8 @@
 package com.pickyourtrail;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.facebook.react.ReactApplication;
 import com.airbnb.android.react.lottie.LottiePackage;
@@ -85,5 +87,11 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+  }
+
+  @Override
+  protected void attachBaseContext(Context base) {
+    super.attachBaseContext(base);
+    MultiDex.install(this);
   }
 }
