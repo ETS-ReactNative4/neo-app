56184100461a6349f531eee66c65e2a6b96ad0cf
dani-PYT <dani@pickyourtrail.com>
2019-06-26 19:35:16 +0530

    created backhandler hoc

---
+++ CommonComponents/BackHandlerHoc/BackHandlerHoc.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { BackHandler } from "react-native";
+
+const BackHandlerHoc = (backHandler = () => false) => WrappedComponent => {
+  return class extends Component {
+    static navigationOptions = WrappedComponent.navigationOptions;
+
+    _didFocusSubscription;
+    _willBlurSubscription;
+
+    constructor(props) {
+      super(props);
+
+      this._didFocusSubscription = props.navigation.addListener(
+        "didFocus",
+        () => {
+          BackHandler.addEventListener("hardwareBackPress", backHandler);
+        }
+      );
+    }
+
+    componentDidMount() {
+      this._willBlurSubscription = this.props.navigation.addListener(
+        "willBlur",
+        () => {
+          BackHandler.removeEventListener("hardwareBackPress", backHandler);
+        }
+      );
+    }
+
+    componentWillUnmount() {
+      this._didFocusSubscription && this._didFocusSubscription.remove();
+      this._willBlurSubscription && this._willBlurSubscription.remove();
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  };
+};
+
+export default BackHandlerHoc;
