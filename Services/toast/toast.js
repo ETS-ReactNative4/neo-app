76654e0d638f38937505a0d1d91657a280f0f468
dani-PYT <dani@pickyourtrail.com>
2019-07-22 19:24:39 +0530

    enable old build system

--- Services/toast/toast.js
+++ Services/toast/toast.js
@@ -1,21 +1,21 @@
-import Toast from "react-native-simple-toast";
+// import Toast from "react-native-simple-toast";
 
 export const toastBottom = text => {
-  Toast.showWithGravity(text, Toast.SHORT, Toast.BOTTOM);
+  // Toast.showWithGravity(text, Toast.SHORT, Toast.BOTTOM);
 };
 
 export const toastCenter = text => {
-  Toast.showWithGravity(text, Toast.SHORT, Toast.CENTER);
+  // Toast.showWithGravity(text, Toast.SHORT, Toast.CENTER);
 };
 
 export const toastTop = text => {
-  Toast.showWithGravity(text, Toast.SHORT, Toast.TOP);
+  // Toast.showWithGravity(text, Toast.SHORT, Toast.TOP);
 };
 
 export const toastLong = text => {
-  Toast.show(text, Toast.LONG);
+  // Toast.show(text, Toast.LONG);
 };
 
 export const toastShort = text => {
-  Toast.show(text, Toast.SHORT);
+  // Toast.show(text, Toast.SHORT);
 };
