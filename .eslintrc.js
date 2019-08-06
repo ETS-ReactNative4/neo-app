67d3869e2538c1e5560872788e116a3056209366
dani-PYT <dani@pickyourtrail.com>
2019-08-06 15:20:26 +0530

    remove corejs and update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
-  extends: "@react-native-community"
+  extends: "@react-native-community",
+  rules: {
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
+    "comma-dangle": 0,
+    "react/prop-types": 1
+  }
 };
