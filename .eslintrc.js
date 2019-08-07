5e5232dc363364a83cb64d9c9a3f99cf196c9a11
dani-PYT <dani@pickyourtrail.com>
2019-08-07 15:14:37 +0530

    make single quotes warnings instead of error

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   extends: "@react-native-community",
   rules: {
-    quotes: ["error", "double", { allowTemplateLiterals: true }],
+    quotes: ["warn", "double", { allowTemplateLiterals: true }],
     "comma-dangle": 0,
     "react/prop-types": 1,
     "prettier/prettier": 1
