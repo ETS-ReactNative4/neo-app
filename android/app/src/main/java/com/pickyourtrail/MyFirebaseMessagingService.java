40ebad3af46ad2ea448c6c10282cabeacd5b1cbb
dani-PYT <dani@pickyourtrail.com>
2019-08-22 13:00:51 +0530

    enable webengage

--- android/app/src/main/java/com/pickyourtrail/MyFirebaseMessagingService.java
+++ android/app/src/main/java/com/pickyourtrail/MyFirebaseMessagingService.java
@@ -2,26 +2,26 @@ package com.pickyourtrail;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-// import com.webengage.sdk.android.WebEngage;
+import com.webengage.sdk.android.WebEngage;
 import java.util.Map;
 import io.invertase.firebase.messaging.RNFirebaseMessagingService;
 
  public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // Map<String, String> data = remoteMessage.getData();
-        // if (data != null) {
-        //     if (data.containsKey("source") && "webengage".equals(data.get("source"))) {
-        //         WebEngage.get().receive(data);
-        //     } else {
+        Map<String, String> data = remoteMessage.getData();
+        if (data != null) {
+            if (data.containsKey("source") && "webengage".equals(data.get("source"))) {
+                WebEngage.get().receive(data);
+            } else {
                 new RNFirebaseMessagingService().onMessageReceived(remoteMessage);
-        //     }
-        // }
+            }
+        }
     }
 
     @Override
     public void onNewToken(String s) {
         super.onNewToken(s);
-        // WebEngage.get().setRegistrationID(s);
+        WebEngage.get().setRegistrationID(s);
     }
 }
