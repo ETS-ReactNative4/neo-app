bd15d95d22fde57cc78cc8df7ff9bb230aca7b34
dani-PYT <dani@pickyourtrail.com>
2019-09-05 13:48:30 +0530

    update info dot component

--- CommonComponents/InfoDot/InfoDot.js
+++ CommonComponents/InfoDot/InfoDot.js
@@ -2,27 +2,26 @@ import React from "react";
 import { View, StyleSheet, ViewPropTypes } from "react-native";
 import constants from "../../constants/constants";
 import PropTypes from "prop-types";
-import forbidExtraProps from "../../Services/PropTypeValidation/forbidExtraProps";
 
-const InfoDot = ({ containerStyle = StyleSheet.create({}), dotRadius = 4 }) => {
+const InfoDot = ({
+  containerStyle = StyleSheet.create({}),
+  dotRadius = 4,
+  color = constants.firstColor
+}) => {
   if (!containerStyle) containerStyle = {};
   const dotStyle = {
     height: dotRadius * 2,
     width: dotRadius * 2,
-    borderRadius: dotRadius
+    borderRadius: dotRadius,
+    backgroundColor: color
   };
-  return <View style={[styles.infoDotContainer, dotStyle, containerStyle]} />;
+  return <View style={[dotStyle, containerStyle]} />;
 };
-
-const styles = StyleSheet.create({
-  infoDotContainer: {
-    backgroundColor: constants.firstColor
-  }
-});
 
 InfoDot.propTypes = {
   containerStyle: ViewPropTypes.style,
-  dotRadius: PropTypes.number
+  dotRadius: PropTypes.number,
+  color: PropTypes.string
 };
 
 export default InfoDot;
