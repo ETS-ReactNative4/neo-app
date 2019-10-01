06b10f24d8f754a9b5d5ed24d46308f03630cada
dani-PYT <dani@pickyourtrail.com>
2019-10-01 19:38:10 +0530

    remove unused files

--- Screens/ActionSheetScreen/ActionSheet.js
+++ Screens/ActionSheetScreen/ActionSheet.js
@@ -1,5 +1,10 @@
 import React, { Component, Fragment, useEffect } from "react";
 
+/**
+ * This component accepts a `viewComponent` as param
+ * If `viewComponent` is not present, it will render an empty fragment which will
+ * automatically closes the ActionSheet
+ */
 class ActionSheet extends Component {
   render() {
     const ViewComponents = this.props.navigation.getParam(
