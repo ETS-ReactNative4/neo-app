75e1bb5c07ac37f594d6e40cfc27398435440d2f
dani-PYT <dani@pickyourtrail.com>
2019-10-11 15:36:14 +0530

    handle normal image scenarios

--- CommonComponents/SmartImage/SmarterImage.js
+++ CommonComponents/SmartImage/SmarterImage.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Image } from "react-native";
+import PropTypes from "prop-types";
 import FastImage from "react-native-fast-image";
+import constants from "../../constants/constants";
 
-const resizeMode = {
+const resizeModeMap = {
   stretch: FastImage.resizeMode.stretch,
   center: FastImage.resizeMode.center,
   cover: FastImage.resizeMode.cover,
   contain: FastImage.resizeMode.contain
 };
 
-const priority = {
+const priorityMap = {
   high: FastImage.priority.high,
   low: FastImage.priority.low,
   normal: FastImage.priority.normal
@@ -17,14 +19,56 @@ const priority = {
 
 const SmartImage = ({
   useFastImage = false,
-  priority = priority.normal,
+  priority = priorityMap.normal,
   resizeMode,
   source,
+  fallbackSource = { uri: constants.defaultPlaceImage },
   ...otherProps
 }) => {
   const ImageComponent = useFastImage ? FastImage : Image;
+  const [imageSource, setImageSource] = useState(source);
+  const [isUsingDefaultImage, setUsingDefaultImage] = useState(false);
 
-  return <ImageComponent />;
+  const [actualImageSource, setActualImageSource] = useState(source);
+  useEffect(() => {
+    if (actualImageSource !== source) {
+      setImageSource(source);
+      setActualImageSource(source);
+      setUsingDefaultImage(false);
+    }
+  }, [actualImageSource, source]);
+
+  let resizeModeProp = resizeMode;
+  let priorityProp = null;
+  if (useFastImage) {
+    resizeModeProp = resizeModeMap[resizeMode];
+    priorityProp = priorityMap[priority];
+  }
+
+  const imageLoadError = () => {
+    if (!isUsingDefaultImage) {
+      setUsingDefaultImage(true);
+      setImageSource(fallbackSource);
+    }
+  };
+
+  return (
+    <ImageComponent
+      {...otherProps}
+      source={imageSource}
+      onError={imageLoadError}
+      resizeMode={resizeModeProp}
+      priority={priorityProp}
+    />
+  );
+};
+
+SmartImage.propTypes = {
+  useFastImage: PropTypes.bool,
+  priority: PropTypes.oneOf(["low", "normal", "high"]),
+  resizeMode: PropTypes.oneOf(["center", "stretch", "cover", "contain"]),
+  source: Image.propTypes.source.isRequired,
+  fallbackSource: Image.propTypes.source.isRequired
 };
 
 export default SmartImage;
--- storybook/stories/SmartImageStory.js
+++ storybook/stories/SmartImageStory.js
@@ -1,4 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
 import { storiesOf } from "@storybook/react-native";
+import SmartImage from "../../CommonComponents/SmartImage/SmarterImage";
+import constants from "../../constants/constants";
 
-storiesOf("Smart Image", module).add("Smart image", () => {});
+const imageSources = {
+  localFile: constants.flightVoucherBanner,
+  networkFile: {
+    uri:
+      "https://images.unsplash.com/photo-1540946485063-a40da27545f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+  },
+  brokenFile: {
+    uri:
+      "https://images.unsplash.com/photo-1540946485063?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+  }
+};
+
+const MultipleScenarios = () => {
+  const images = [
+    imageSources.localFile,
+    imageSources.networkFile,
+    imageSources.brokenFile
+  ];
+  const [index, setIndex] = useState(0);
+  let _interval;
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    _interval = setInterval(() => {
+      if (index < images.length - 1) {
+        setIndex(index + 1);
+      } else {
+        setIndex(0);
+      }
+    }, 2500);
+    return () => {
+      clearInterval(_interval);
+    };
+  });
+  return (
+    <View style={styles.container}>
+      <SmartImage style={styles.imageContainer} source={images[index]} />
+    </View>
+  );
+};
+
+storiesOf("Smart Image", module)
+  .add("Smart image - local image file", () => {
+    return (
+      <View style={styles.container}>
+        <SmartImage
+          style={styles.imageContainer}
+          source={imageSources.localFile}
+        />
+      </View>
+    );
+  })
+  .add("Smart image - network image file", () => {
+    return (
+      <View style={styles.container}>
+        <SmartImage
+          style={styles.imageContainer}
+          source={imageSources.networkFile}
+        />
+      </View>
+    );
+  })
+  .add("Smart image - broken image file", () => {
+    return (
+      <View style={styles.container}>
+        <SmartImage
+          style={styles.imageContainer}
+          source={imageSources.brokenFile}
+        />
+      </View>
+    );
+  })
+  .add("Smart image - all three scenarios", () => {
+    return <MultipleScenarios />;
+  });
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  imageContainer: {
+    height: 150,
+    width: 150
+  }
+});
