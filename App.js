0b589fcdbd4f415943cd0c7e64275d93de1dbfe7
dani-PYT <dani@pickyourtrail.com>
2019-10-31 18:19:53 +0530

    enable analytics in staging

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ class App extends Component {
     UIManager.setLayoutAnimationEnabledExperimental &&
       UIManager.setLayoutAnimationEnabledExperimental(true);
 
-    if (!__DEV__ && isProduction()) {
+    if (!__DEV__) {
       enableAnalytics();
     } else {
       disableAnalytics();
