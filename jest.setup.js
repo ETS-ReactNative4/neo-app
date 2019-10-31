acbe09c55a70e37a3965bf89a8d5387b6b290a71
GP <gp@pickyourtrail.com>
2019-10-31 12:30:47 +0530

    jest: Also mock RNAnalytics so we can test it via Jest.

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,13 @@
 import { NativeModules } from "react-native";
 
+// Mock RNCNetInfo
 NativeModules.RNCNetInfo = {
   getCurrentState: jest.fn(() => Promise.resolve()),
   addListener: jest.fn(),
   removeListeners: jest.fn()
 };
+
+// Mock RNAnalytics (see https://github.com/segmentio/analytics-react-native/issues/32)
+NativeModules.RNAnalytics = {};
+const mockAnalytics = jest.genMockFromModule("@segment/analytics-react-native");
+jest.mock("@segment/analytics-react-native", () => mockAnalytics);
