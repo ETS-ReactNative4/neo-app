eadcc99d32faba0adc2261c78d1fb1fb1f7b3882
dani-PYT <dani@pickyourtrail.com>
2019-11-06 11:56:19 +0530

    migrate fab button to tsx

--- CommonComponents/FabButton/FabButton.tsx
+++ CommonComponents/FabButton/FabButton.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import {
-  View,
-  StyleSheet,
-  ViewPropTypes,
-  TouchableOpacity
-} from "react-native";
-import PropTypes from "prop-types";
+import { StyleSheet, ViewStyle, TouchableOpacity } from "react-native";
 import constants from "../../constants/constants";
 import Icon from "../Icon/Icon";
+
+export interface FabButtonProps {
+  containerStyle: ViewStyle;
+  radius: number;
+  backgroundColor: string;
+  iconColor: string;
+  action: () => void;
+  icon: string;
+  iconSize: string;
+}
 
 const FabButton = ({
   containerStyle = StyleSheet.create({}),
@@ -17,7 +21,7 @@ const FabButton = ({
   action = () => null,
   icon,
   iconSize
-}) => {
+}: FabButtonProps) => {
   const buttonContainer = {
     height: radius * 2,
     width: radius * 2,
@@ -34,17 +38,11 @@ const FabButton = ({
   );
 };
 
-FabButton.propTypes = {
-  containerStyle: ViewPropTypes.style,
-  radius: PropTypes.number.isRequired,
-  backgroundColor: PropTypes.string,
-  iconColor: PropTypes.string,
-  action: PropTypes.func.isRequired,
-  icon: PropTypes.string.isRequired,
-  iconSize: PropTypes.string.isRequired
-};
+export interface FabButtonStyles {
+  fabButtonContainer: ViewStyle;
+}
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create<FabButtonStyles>({
   fabButtonContainer: {
     alignItems: "center",
     justifyContent: "center"
