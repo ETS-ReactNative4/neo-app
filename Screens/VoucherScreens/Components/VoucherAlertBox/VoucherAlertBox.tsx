8f3f806410cb553d0b010429be931a01743d0e24
dani-PYT <dani@pickyourtrail.com>
2019-11-11 17:51:13 +0530

    created voucher alert box

--- Screens/VoucherScreens/Components/VoucherAlertBox/VoucherAlertBox.tsx
+++ Screens/VoucherScreens/Components/VoucherAlertBox/VoucherAlertBox.tsx
@@ -1,30 +1,76 @@
 import React from "react";
-import { View, StyleSheet, ViewStyle, Text } from "react-native";
+import { View, StyleSheet, ViewStyle, Text, TextStyle } from "react-native";
 import Icon from "../../../../CommonComponents/Icon/Icon";
+import { CONSTANT_alertFilledIcon } from "../../../../constants/imageAssets";
+import constants from "../../../../constants/constants";
+import changeColorAlpha from "../../../../Services/changeColorAlpha/changeColorAlpha";
 
 export interface VoucherAlertBoxProps {
   containerStyle?: ViewStyle;
   alertText: string;
+  mode: "alert" | "info";
 }
 
 const VoucherAlertBox = ({
   containerStyle = StyleSheet.create({}),
-  alertText
+  alertText = "",
+  mode = "alert"
 }: VoucherAlertBoxProps) => {
+  if (!alertText) return null;
+
+  let color = constants.seventeenthColor,
+    iconName = CONSTANT_alertFilledIcon;
+
+  if (mode === "alert") {
+    color = constants.seventeenthColor;
+    iconName = CONSTANT_alertFilledIcon;
+  }
+  if (mode === "info") {
+    color = constants.fifteenthColor;
+    iconName = constants.checkMarkCircle;
+  }
+
   return (
-    <View style={[styles.voucherAlertBoxContainer, containerStyle]}>
-      <Icon />
-      <Text>{}</Text>
+    <View
+      style={[
+        styles.voucherAlertBoxContainer,
+        { backgroundColor: changeColorAlpha(color, 0.3) },
+        containerStyle
+      ]}
+    >
+      <View style={styles.iconContainer}>
+        <Icon name={iconName} size={14} color={color} />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={[styles.alertText, { color }]}>{alertText}</Text>
+      </View>
     </View>
   );
 };
 
 export interface VoucherAlertBoxStyles {
   voucherAlertBoxContainer: ViewStyle;
+  iconContainer: ViewStyle;
+  textContainer: ViewStyle;
+  alertText: TextStyle;
 }
 
 const styles = StyleSheet.create<VoucherAlertBoxStyles>({
-  voucherAlertBoxContainer: {}
+  voucherAlertBoxContainer: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    padding: 24
+  },
+  iconContainer: {
+    marginTop: 4
+  },
+  textContainer: {
+    marginLeft: 8
+  },
+  alertText: {
+    ...constants.fontCustom(constants.primaryRegular, 14, 20)
+  }
 });
 
 export default VoucherAlertBox;
--- Screens/VoucherScreens/Components/VoucherAlertBox/VoucherAlertBoxTestCases.js
+++ Screens/VoucherScreens/Components/VoucherAlertBox/VoucherAlertBoxTestCases.js
@@ -4,7 +4,25 @@ import VoucherAlertBox from "./VoucherAlertBox";
 const VoucherAlertBoxTestCases = [
   {
     title: "Voucher Alert Box",
-    Component: <VoucherAlertBox />
+    Component: (
+      <VoucherAlertBox
+        alertText={
+          "As the transfer is on shared basis, the arrival time of transfer could vary plus or minus 15-30minutes depending on traffic"
+        }
+        mode={"alert"}
+      />
+    )
+  },
+  {
+    title: "Voucher Alert Box - info mode",
+    Component: (
+      <VoucherAlertBox
+        alertText={
+          "As the transfer is on shared basis, the arrival time of transfer could vary plus or minus 15-30minutes depending on traffic"
+        }
+        mode={"info"}
+      />
+    )
   }
 ];
 
