02a6155c62fc3db23558c7521e2a52659fc22d8b
dani-PYT <dani@pickyourtrail.com>
2019-11-27 11:12:04 +0530

    Important config changes for typescript
    ⚙️config update:
    - Typescript & ESLint checks have been made mandatory for staged files
    - Using .huskyrc.json instead of precommit for the pre-commit hooks
    
    📦package updates:
    - freshchat react-native sdk
    - husky
    - jetifier
    - pretty-quick

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,19 @@
 module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json"
+  },
+  plugins: ["@typescript-eslint", "tsc"],
   root: true,
   extends: "@react-native-community",
   rules: {
     quotes: ["warn", "double", { allowTemplateLiterals: true }],
+    "tsc/config": [
+      1,
+      {
+        configFile: "tsconfig.json"
+      }
+    ],
     "comma-dangle": 0,
     "react/prop-types": 1,
     "prettier/prettier": 1,
