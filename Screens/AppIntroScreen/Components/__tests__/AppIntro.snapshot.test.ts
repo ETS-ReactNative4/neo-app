ccebe54d9fcddd218438a2645c667d8ba1136965
dani-PYT <dani@pickyourtrail.com>
2020-01-27 12:00:30 +0530

    added snapshot tests to feedback screens

---
+++ Screens/AppIntroScreen/Components/__tests__/AppIntro.snapshot.test.ts
@@ -0,0 +1,4 @@
+import testRunner from "../../../../Services/testRunner/testRunner";
+import AppIntroComponentTestCases from "../AppIntroComponentTestCases";
+
+AppIntroComponentTestCases.map(testRunner);
