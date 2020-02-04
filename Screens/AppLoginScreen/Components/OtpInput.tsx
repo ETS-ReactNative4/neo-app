4c8260d46b820246c3aac221d9e06d7c6ee47545
dani-PYT <dani@pickyourtrail.com>
2020-02-04 11:54:54 +0530

    create otp panel component

--- Screens/AppLoginScreen/Components/OtpInput.tsx
+++ Screens/AppLoginScreen/Components/OtpInput.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform } from "react-native";
 // @ts-ignore
 import OTPInputView from "@twotalltotems/react-native-otp-input";
 import Icon from "../../../CommonComponents/Icon/Icon";
 import { CONSTANT_changeIcon } from "../../../constants/imageAssets";
 import {
   CONSTANT_shade1,
-  CONSTANT_shade4,
-  CONSTANT_black1
+  CONSTANT_shade4
 } from "../../../constants/colorPallete";
 import {
   CONSTANT_fontCustom,
@@ -18,12 +17,14 @@ export interface OtpInputProps {
   code: string;
   onInputChange: (text: string) => any;
   onCodeFilled: (text: string) => any;
+  pinCount?: number;
 }
 
 const OtpInput = ({
   code = "",
   onInputChange = () => null,
-  onCodeFilled = () => null
+  onCodeFilled = () => null,
+  pinCount = 6
 }: OtpInputProps) => {
   const onCodeChange = (changedCode: string) => {
     onInputChange(changedCode);
@@ -32,11 +33,11 @@ const OtpInput = ({
   return (
     <View style={styles.otpInputContainer}>
       <View style={styles.iconWrapper}>
-        <Icon name={CONSTANT_changeIcon} size={20} color={CONSTANT_shade1} />
+        <Icon name={CONSTANT_changeIcon} size={30} color={CONSTANT_shade1} />
       </View>
       <View style={styles.otpWrapper}>
         <OTPInputView
-          pinCount={6}
+          pinCount={pinCount}
           code={code}
           onCodeChanged={onCodeChange}
           codeInputFieldStyle={[
@@ -55,7 +56,14 @@ const OtpInput = ({
 const styles = StyleSheet.create({
   otpInputContainer: {
     flexDirection: "row",
-    height: 40,
+    ...Platform.select({
+      ios: {
+        height: 40
+      },
+      android: {
+        height: 62
+      }
+    }),
     alignItems: "center",
     borderBottomColor: CONSTANT_shade4,
     borderBottomWidth: 1
@@ -70,7 +78,14 @@ const styles = StyleSheet.create({
   otpInputField: {
     ...CONSTANT_fontCustom(CONSTANT_primarySemiBold, 30),
     lineHeight: undefined,
-    color: CONSTANT_black1
+    color: CONSTANT_shade1,
+    marginBottom: 8,
+    ...Platform.select({
+      android: {
+        marginTop: 8,
+        height: 62
+      }
+    })
   },
   underlineStyleBase: {
     borderWidth: 0
---
+++ Screens/AppLoginScreen/Components/OtpPanel.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import OtpInput from "./OtpInput";
+import {
+  CONSTANT_fontCustom,
+  CONSTANT_primarySemiBold
+} from "../../../constants/fonts";
+import {
+  CONSTANT_black1,
+  CONSTANT_shade1,
+  CONSTANT_fifteenthColor
+} from "../../../constants/colorPallete";
+
+const OtpPanel = ({}) => {
+  const [code, setCode] = useState<string>("");
+  const updateCode = (newCode: string) => {
+    setCode(newCode);
+  };
+  const onResendClick = () => null;
+
+  return (
+    <View style={styles.otpPanelContainer}>
+      <Text style={styles.infoHeaderText}>{"AUTHENTICATE"}</Text>
+      <Text style={styles.infoText}>
+        {"We’ve sent you a 6 digit one time password for security."}
+      </Text>
+      <OtpInput
+        code={code}
+        onInputChange={updateCode}
+        onCodeFilled={updateCode}
+      />
+      <View style={styles.optionsSection}>
+        <View>
+          <Text style={[styles.optionText, styles.leftText]}>
+            {"TIMING OUT IN"}
+          </Text>
+          <Text style={[styles.optionText, styles.leftText]}>
+            {"1:00 MINS"}
+          </Text>
+        </View>
+        <View>
+          <Text style={[styles.optionText, styles.rightText]}>
+            {"DIDN’T RECEIVE IT?"}
+          </Text>
+          <Text
+            onPress={onResendClick}
+            style={[styles.optionText, styles.rightText, styles.textLink]}
+          >
+            {"RESEND OTP"}
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  otpPanelContainer: {},
+  infoHeaderText: {
+    ...CONSTANT_fontCustom(CONSTANT_primarySemiBold, 10),
+    color: CONSTANT_black1,
+    marginBottom: 8
+  },
+  infoText: {
+    ...CONSTANT_fontCustom(CONSTANT_primarySemiBold, 20),
+    color: CONSTANT_black1,
+    marginBottom: 32
+  },
+  optionsSection: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: 16
+  },
+  optionText: {
+    ...CONSTANT_fontCustom(CONSTANT_primarySemiBold, 10, 15),
+    color: CONSTANT_shade1
+  },
+  rightText: {
+    textAlign: "right"
+  },
+  leftText: {
+    textAlign: "left"
+  },
+  textLink: {
+    color: CONSTANT_fifteenthColor
+  }
+});
+
+export default OtpPanel;
