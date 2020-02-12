1e53489b67f10af19b6009c6ee9f623c9ca505af
dani-PYT <dani@pickyourtrail.com>
2020-02-12 16:34:32 +0530

    removed switch case from server urls

--- constants/serverUrls.js
+++ constants/serverUrls.js
@@ -4,40 +4,23 @@ import {
 } from "../Services/getEnvironmentDetails/getEnvironmentDetails";
 
 const productUrls = {
-  localProductUrl: "http://192.168.0.5:8080/",
-  stagingProductUrl: "https://delta.longweekend.co.in/",
-  prodProductUrl: "https://pickyourtrail.com/",
-  testingProdUrl: "https://dummy-testing-server.com/"
+  local: "http://192.168.0.5:8080/",
+  staging: "https://delta.longweekend.co.in/",
+  production: "https://pickyourtrail.com/",
+  test: "https://dummy-testing-server.com/"
 };
 
 const apiServers = {
-  localServer: productUrls.localProductUrl + "api/",
-  stagingSever: productUrls.stagingProductUrl + "api/",
-  prodServer: productUrls.prodProductUrl + "api/",
-  testingServer: productUrls.testingProdUrl + "api/"
+  local: productUrls.local + "api/",
+  staging: productUrls.staging + "api/",
+  production: productUrls.production + "api/",
+  test: productUrls.test + "api/"
 };
 
-let apiServerUrl, productUrl;
-switch (getEnvironmentName()) {
-  case "production":
-    apiServerUrl = apiServers.prodServer;
-    productUrl = productUrls.prodProductUrl;
-    break;
+const ENVIRONMENT = getEnvironmentName();
 
-  case "staging":
-    apiServerUrl = apiServers.stagingSever;
-    productUrl = productUrls.stagingProductUrl;
-    break;
-
-  case "test":
-    apiServerUrl = apiServers.testingServer;
-    productUrl = productUrls.testingProdUrl;
-    break;
-
-  default:
-    apiServerUrl = apiServers.localServer;
-    productUrl = productUrls.localProductUrl;
-}
+const apiServerUrl = apiServers[ENVIRONMENT] || apiServers.local;
+const productUrl = productUrls[ENVIRONMENT] || productUrls.local;
 
 const chatCustomUrl = `https://pickyourtrail.com/app-chat/`;
 
