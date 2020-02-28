52f89993eef2120862ce04b0efa59ca8edbfa834
suhailpyt <capamerica@pickyourtrail.com>
2020-02-28 11:17:34 +0530

    Set marital status card ratio

--- Screens/MaritalStatusScreen/Components/MaritalStatusCard.tsx
+++ Screens/MaritalStatusScreen/Components/MaritalStatusCard.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-  View,
   StyleSheet,
   Text,
   ViewStyle,
@@ -10,7 +9,7 @@ import {
 } from "react-native";
 import {
   CONSTANT_black1,
-  CONSTANT_shade5,
+  CONSTANT_white,
   CONSTANT_shade1
 } from "../../../constants/colorPallete";
 import {
@@ -19,6 +18,7 @@ import {
 } from "../../../constants/fonts";
 import SmartImageV2 from "../../../CommonComponents/SmartImage/SmartImageV2";
 import { CONSTANT_defaultPlaceImage } from "../../../constants/imageAssets";
+import ratioCalculator from "../../../Services/ratioCalculator/ratioCalculator";
 
 export interface MaritalStatusCardProps {
   imageSource: string;
@@ -27,7 +27,16 @@ export interface MaritalStatusCardProps {
   imageStyle?: StyleProp<ImageStyle>;
   onPress: () => any;
   isSelected: boolean;
+  maritalStatusCardWidth: number;
+  maritalStatusCardHeight: number;
 }
+
+const MARITAL_STATUS_CARD_IMAGE_WIDTH = 68;
+const MARITAL_STATUS_CARD_IMAGE_HEIGHT = ratioCalculator(
+  136,
+  98,
+  MARITAL_STATUS_CARD_IMAGE_WIDTH
+);
 
 const MaritalStatusCard = ({
   containerStyle,
@@ -35,53 +44,55 @@ const MaritalStatusCard = ({
   text = "",
   imageStyle,
   onPress = () => null,
-  isSelected
+  isSelected,
+  maritalStatusCardWidth,
+  maritalStatusCardHeight
 }: MaritalStatusCardProps) => {
   const selectedAction = () => {
     onPress();
   };
 
+  const cardHeight = {
+    width: maritalStatusCardWidth,
+    height: maritalStatusCardHeight
+  };
+
   return (
-    <View style={[styles.cardContainerStyle, containerStyle]}>
-      <TouchableOpacity
-        activeOpacity={0.8}
-        onPress={selectedAction}
-        style={styles.touchableOpacityStyle}
-      >
-        <SmartImageV2
-          resizeMode={"cover"}
-          source={{ uri: imageSource }}
-          fallbackSource={{ uri: CONSTANT_defaultPlaceImage }}
-          style={[
-            styles.image,
-            !isSelected ? styles.unselectedImage : null,
-            imageStyle
-          ]}
-        />
-        <Text style={styles.textStyle}>{text}</Text>
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={selectedAction}
+      style={[styles.cardContainerStyle, cardHeight, containerStyle]}
+    >
+      <SmartImageV2
+        resizeMode={"cover"}
+        source={{ uri: imageSource }}
+        fallbackSource={{ uri: CONSTANT_defaultPlaceImage }}
+        style={[
+          styles.image,
+          !isSelected ? styles.unselectedImage : null,
+          imageStyle
+        ]}
+      />
+      <Text style={styles.textStyle}>{text}</Text>
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   cardContainerStyle: {
-    backgroundColor: CONSTANT_shade5,
+    backgroundColor: CONSTANT_white,
     borderRadius: 4,
-    height: 186,
     justifyContent: "center",
+    alignItems: "center",
     marginBottom: 8
-  },
-  touchableOpacityStyle: {
-    alignItems: "center"
   },
   textStyle: {
     color: CONSTANT_black1,
     ...CONSTANT_fontCustom(CONSTANT_primaryRegular, 16)
   },
   image: {
-    width: 68,
-    height: 50,
+    width: MARITAL_STATUS_CARD_IMAGE_WIDTH,
+    height: MARITAL_STATUS_CARD_IMAGE_HEIGHT,
     marginBottom: 32
   },
   unselectedImage: {
