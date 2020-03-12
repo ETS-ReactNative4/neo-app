b663fc517ac0920e64acc3fd61e93fa45cac7185
dani-PYT <dani@pickyourtrail.com>
2020-03-12 14:33:16 +0530

    created notifications tab item

--- NavigatorsV2/ScreenNames.ts
+++ NavigatorsV2/ScreenNames.ts
@@ -6,6 +6,7 @@ export const SCREEN_TRAVEL_MARITAL_STATUS = "TravelMaritalStatus";
 export const SCREEN_PRETRIP_HOME_TABS = "PreTripHomeTabs";
 export const SCREEN_EXPLORE_TAB = "ExploreTab";
 export const SCREEN_SEARCH_TAB = "SearchTab";
+export const SCREEN_NOTIFICATION_TAB = "NotificationTab";
 export const SCREEN_EXPLORE_PAGE = "ExplorePage";
 export const SCREEN_LISTING_PAGE = "ListingPage";
 export const SCREEN_MODAL_STACK = "ModalStack";
