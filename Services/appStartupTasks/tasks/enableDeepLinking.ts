5982a570e632c36fdba0d743a1ca353daaaacfff
dani-PYT <dani@pickyourtrail.com>
2020-05-20 11:07:46 +0530

    validate uri in branch deeplinking

--- Services/appStartupTasks/tasks/enableDeepLinking.ts
+++ Services/appStartupTasks/tasks/enableDeepLinking.ts
@@ -10,31 +10,33 @@ export interface IBranchSubscribeParams {
 }
 
 const enableDeepLinking = () => {
-  branch.subscribe(({ error, params }: IBranchSubscribeParams) => {
-    if (error) {
-      logError(error, {
-        type: "Unable to Deeplink with Branch"
-      });
-      return;
-    }
+  branch.subscribe(({ error, params, uri }: IBranchSubscribeParams) => {
+    if (uri) {
+      if (error) {
+        logError(error, {
+          type: "Unable to Deeplink with Branch"
+        });
+        return;
+      }
 
-    try {
-      const meta = params.$custom_meta_tags
-        ? JSON.parse(params.$custom_meta_tags)
-        : "";
+      try {
+        const meta = params.$custom_meta_tags
+          ? JSON.parse(params.$custom_meta_tags)
+          : "";
 
-      const screen = params.screen;
+        const screen = params.screen;
 
-      if (screen) {
-        deepLink({
-          link: screen,
-          screenData: meta
+        if (screen) {
+          deepLink({
+            link: screen,
+            screenData: meta
+          });
+        }
+      } catch (e) {
+        logError(e, {
+          type: "Unable to parse deeplink url"
         });
       }
-    } catch (e) {
-      logError(e, {
-        type: "Unable to parse deeplink url"
-      });
     }
   });
 };
