65332996d85edbbd03a5fb6e32b8007da93e35c1
dani-PYT <dani@pickyourtrail.com>
2020-07-15 16:22:01 +0530

    feat(getpricewithoutsymbol.tsx): generate global price without symbols

--- Screens/ExploreScreen/services/getPriceWithoutSymbol.ts
+++ Screens/ExploreScreen/services/getPriceWithoutSymbol.ts
@@ -1,4 +1,12 @@
 import getLocaleString from "../../../Services/getLocaleString/getLocaleString";
+import { getLocales, findBestAvailableLanguage } from "react-native-localize";
+
+const locales = getLocales();
+
+const languageCodes = locales.map(each => each.languageCode);
+
+const systemLanguageCode =
+  findBestAvailableLanguage(languageCodes)?.languageTag ?? "en-IN";
 
 // PT TODO: Better way to display the money...
 const getPriceWithoutSymbol = (price: number) => {
@@ -8,6 +16,40 @@ const getPriceWithoutSymbol = (price: number) => {
     ? amountWithoutRupeeSymbol.slice(0, -3)
     : amountWithoutRupeeSymbol;
   return amountWithoutFixedDecimals;
+};
+
+// Based on https://stackoverflow.com/a/58653059/5597641
+
+function getCurrencySymbol(locale: string, currency: string) {
+  return (0)
+    .toLocaleString(locale, {
+      style: "currency",
+      currency: currency,
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0
+    })
+    .replace(/\d/g, "")
+    .trim();
+}
+
+export const getGlobalPriceWithoutSymbol = ({
+  amount,
+  languageCode = systemLanguageCode,
+  currency = "INR"
+}: {
+  amount: number;
+  languageCode?: string;
+  currency?: string;
+}) => {
+  const amountFormatter = new Intl.NumberFormat(languageCode, {
+    style: "currency",
+    currency,
+    currencyDisplay: "symbol"
+  });
+  const localCurrencySymbol = getCurrencySymbol(languageCode, currency);
+  let amountString = amountFormatter.format(amount);
+  amountString = amountString.replace(localCurrencySymbol, "");
+  return amountString;
 };
 
 export default getPriceWithoutSymbol;
