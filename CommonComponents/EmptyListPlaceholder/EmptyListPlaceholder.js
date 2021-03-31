1de15ef5f1958d732b134696c25895f5e418329b
priyanka pyt <priyanka@pickyourtrail.com>
2021-03-31 19:34:17 +0530

    fixed height issues in ios

--- CommonComponents/EmptyListPlaceholder/EmptyListPlaceholder.js
+++ CommonComponents/EmptyListPlaceholder/EmptyListPlaceholder.js
@@ -1,12 +1,16 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
-import PropTypes from "prop-types";
-import constants from "../../constants/constants";
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import PropTypes from 'prop-types';
+import constants from '../../constants/constants';
 
-const EmptyListPlaceholder = ({ text, containerStyle, textStyle }) => {
-  if (!containerStyle) containerStyle = {};
+const EmptyListPlaceholder = ({text, containerStyle, textStyle}) => {
+  if (!containerStyle) {
+    containerStyle = {};
+  }
 
-  if (!textStyle) textStyle = {};
+  if (!textStyle) {
+    textStyle = {};
+  }
 
   return (
     <View style={[styles.placeholderContainer, containerStyle]}>
@@ -18,19 +22,20 @@ const EmptyListPlaceholder = ({ text, containerStyle, textStyle }) => {
 EmptyListPlaceholder.propTypes = {
   text: PropTypes.string.isRequired,
   containerStyle: PropTypes.object,
-  textStyle: PropTypes.object
+  textStyle: PropTypes.object,
 };
 
 const styles = StyleSheet.create({
   placeholderContainer: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: 50,
   },
   placeholderText: {
     ...constants.font17(constants.primaryLight),
-    color: constants.shade1
-  }
+    color: constants.shade1,
+  },
 });
 
 export default EmptyListPlaceholder;
