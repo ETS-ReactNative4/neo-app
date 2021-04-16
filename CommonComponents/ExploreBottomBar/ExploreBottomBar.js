3881fc4f50032d1172dd337de9bb5b778f5c1505
keerthi <keerthi@pickyourtrail.com>
2021-04-16 19:22:48 +0530

    Added chat in pretrip

--- CommonComponents/ExploreBottomBar/ExploreBottomBar.js
+++ CommonComponents/ExploreBottomBar/ExploreBottomBar.js
@@ -1,25 +1,30 @@
-import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import PropTypes from "prop-types";
-import BottomBarWrapper from "../BottomBarWrapper/BottomBarWrapper";
-import Icon from "../Icon/Icon";
+import React from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
+import BottomBarWrapper from '../BottomBarWrapper/BottomBarWrapper';
+import Icon from '../Icon/Icon';
 import {
   CONSTANT_fontCustom,
-  CONSTANT_primaryRegular
-} from "../../constants/fonts";
-import { CONSTANT_white } from "../../constants/colorPallete";
-import { CONSTANT_dealsFlare } from "../../constants/imageAssets";
-import LottieView from "lottie-react-native";
+  CONSTANT_primaryRegular,
+} from '../../constants/fonts';
+import {CONSTANT_white} from '../../constants/colorPallete';
+import {
+  CONSTANT_dealsFlare,
+  CONSTANT_chatIcon,
+} from '../../constants/imageAssets';
+import LottieView from 'lottie-react-native';
+import {chatLauncher} from '../../Services/freshchatService/freshchatService';
+import SmartImageV2 from '../SmartImage/SmartImageV2';
 
 /**
  * PT TODO: Typing unavailable for bottom bar hence the file is in js
  * Must be updated once typings are updated
  */
-const ExploreBottomBar = ({ state, descriptors, navigation }) => {
+const ExploreBottomBar = ({state, descriptors, navigation}) => {
   return (
     <View style={styles.bottomBarContainer}>
       {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key];
+        const {options} = descriptors[route.key];
         const label =
           options.tabBarLabel !== undefined
             ? options.tabBarLabel
@@ -31,19 +36,23 @@ const ExploreBottomBar = ({ state, descriptors, navigation }) => {
 
         const onPress = () => {
           const event = navigation.emit({
-            type: "tabPress",
-            target: route.key
+            type: 'tabPress',
+            target: route.key,
           });
 
           if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name);
+            if (label === 'Chat') {
+              chatLauncher();
+            } else {
+              navigation.navigate(route.name);
+            }
           }
         };
 
         const onLongPress = () => {
           navigation.emit({
-            type: "tabLongPress",
-            target: route.key
+            type: 'tabLongPress',
+            target: route.key,
           });
         };
 
@@ -53,7 +62,7 @@ const ExploreBottomBar = ({ state, descriptors, navigation }) => {
         const buttonContainerStyle = {
           backgroundColor: isFocused
             ? options.activeBackgroundColor
-            : options.inactiveBackgroundColor
+            : options.inactiveBackgroundColor,
         };
 
         return (
@@ -61,16 +70,20 @@ const ExploreBottomBar = ({ state, descriptors, navigation }) => {
             key={index}
             activeOpacity={0.8}
             accessibilityRole="button"
-            accessibilityStates={isFocused ? ["selected"] : []}
+            accessibilityStates={isFocused ? ['selected'] : []}
             accessibilityLabel={options.tabBarAccessibilityLabel}
             testID={options.tabBarTestID}
             onPress={onPress}
             onLongPress={onLongPress}
-            style={[styles.buttonContainer, buttonContainerStyle]}
-          >
+            style={[styles.buttonContainer, buttonContainerStyle]}>
             <View style={styles.iconContainer}>
-              {label === "Deals" && !isFocused ? (
+              {label === 'Deals' && !isFocused ? (
                 <LottieView source={CONSTANT_dealsFlare()} autoPlay loop />
+              ) : label === 'Chat' ? (
+                <SmartImageV2
+                  source={CONSTANT_chatIcon()}
+                  style={styles.chatStyle}
+                />
               ) : (
                 <Icon name={options.icon} color={color} size={24} />
               )}
@@ -86,31 +99,32 @@ const ExploreBottomBar = ({ state, descriptors, navigation }) => {
 ExploreBottomBar.propTypes = {
   state: PropTypes.object,
   descriptors: PropTypes.object,
-  navigation: PropTypes.object
+  navigation: PropTypes.object,
 };
 
 const styles = StyleSheet.create({
   bottomBarContainer: {
     flex: 1,
-    flexDirection: "row",
-    backgroundColor: "transparent"
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
   },
   buttonContainer: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   iconContainer: {
     height: 32,
     width: 32,
-    alignItems: "center",
-    justifyContent: "center"
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   text: {
     ...CONSTANT_fontCustom(CONSTANT_primaryRegular, 12),
     marginTop: 8,
-    color: CONSTANT_white
-  }
+    color: CONSTANT_white,
+  },
+  chatStyle: {width: 22, height: 22, opacity: 0.4, marginTop: 4},
 });
 
 export default BottomBarWrapper(ExploreBottomBar);
